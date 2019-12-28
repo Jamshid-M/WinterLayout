9d8b36f70f13009e6122106bd1798c3a796cae92
jamshid <Jama9779@gmail.com>
2019-12-28 11:56:02 +0500

    extra gif

--- app/src/main/java/uz/jamshid/winterlayout/MainActivity.kt
+++ app/src/main/java/uz/jamshid/winterlayout/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        winter.setSnowSize(20)
+//        winter.setSnowSize(20)
     }
 
     fun start(view: View) {
